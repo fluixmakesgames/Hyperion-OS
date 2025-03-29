71d58371bb6e0fa9b11971f8e03c1c58584973fa
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-03-28 19:14:32 -0700

    Made build.sh clean up the .o files and kernel.bin
    this is soooo good daddeh

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@ gcc -m32 -c kernel.c -o kernel.o -fno-stack-protector
 gcc -m32 -c ./src/print.c -o ./src/print.o
 ld -m elf_i386 -T link.ld -o ./bin/kernel.bin boot.o kernel.o ./src/print.o
 qemu-system-x86_64 -kernel ./bin/kernel.bin
+
+# Cleaning
+rm ./kernel.o
+rm ./boot.o
+rm ./bin/kernel.bin
